dd39e5c7d9f0d7b1fe60040886d192f3d3962639
sebi-byzantium-pc <sebastiantellob96@hotmail.com>
2026-05-29 00:13:30 -0600

    fixed gauge positioning

--- src/main/java/org/jtop/ui/CpuPanel.java
+++ src/main/java/org/jtop/ui/CpuPanel.java
@@ -3,20 +3,33 @@ package org.jtop.ui;
 import dev.tamboui.toolkit.element.Element;
 import org.jtop.model.SystemSnapshot;
 
-import static dev.tamboui.toolkit.Toolkit.gauge;
-import static dev.tamboui.toolkit.Toolkit.panel;
+import static dev.tamboui.toolkit.Toolkit.*;
 
 public class CpuPanel {
 
     public Element render(SystemSnapshot systemSnapshot) {
         int cores = systemSnapshot.cpuLoadPerCore().length;
-        Element[] gauges = new Element[cores];
+        int half = cores / 2;
+        Element[] left = new Element[half];
+        Element[] right = new Element[cores - half];
 
-        for( int i = 0; i < cores; i++ ) {
-            gauges[i] = gauge(systemSnapshot.cpuLoadPerCore()[i]).label("CPU" + (i + 1));
+        for( int i = 0; i < half; i++ ) {
+            left[i] = row(
+                    text(String.format("%-6s", "CPU" + (i + 1))),
+                    gauge(systemSnapshot.cpuLoadPerCore()[i])
+                            .label(String.format("%5.1f%%", systemSnapshot.cpuLoadPerCore()[i] * 100))
+            );
         }
 
-        return panel("CPU", gauges).rounded();
+        for ( int i = half; i < cores; i++ ) {
+            right[i - half] = row(
+                    text(String.format("%-6s", "CPU" + (i + 1))),
+                    gauge(systemSnapshot.cpuLoadPerCore()[i])
+                            .label(String.format("%5.1f%%", systemSnapshot.cpuLoadPerCore()[i] * 100))
+            );
+        }
+
+        return panel("CPU", row(column(left), spacer(), column(right))).rounded();
     }
 
 }
